95d51ff2c9794adfcf5b1ae30adbc301a1da23f4
Lexwtf <120677031+Lexwtf@users.noreply.github.com>
2026-04-17 22:52:38 +0900

    fix KilaUra

--- src/main/java/com/github/kaguya/module/modules/KillAura.java
+++ src/main/java/com/github/kaguya/module/modules/KillAura.java
@@ -56,9 +56,9 @@ import java.util.Random;
 public class KillAura extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
     private static final DecimalFormat df = new DecimalFormat("+0.0;-0.0", new DecimalFormatSymbols(Locale.US));
-    private static final Color TARGET_IDLE_COLOR = new Color(228, 93, 93, 128);
-    private static final Color TARGET_HIT_COLOR = new Color(228, 93, 93, 150 );
-    private static final Color TARGET_DAMAGE_COLOR = new Color(255, 10, 10 ,200);
+    private static final Color TARGET_IDLE_COLOR = new Color(255, 10, 10 ,10);
+    private static final Color TARGET_HIT_COLOR = new Color(255, 10, 10 ,70);
+    private static final Color TARGET_DAMAGE_COLOR = new Color(255, 10, 10 ,100);
     private static final int HIT_COLOR_TICKS = 6;
     private final TimerUtil timer = new TimerUtil();
     private AttackData target = null;

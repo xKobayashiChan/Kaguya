ff0a2e958e20dcec3abdb563c38900b1915ab6c2
Lexwtf <120677031+Lexwtf@users.noreply.github.com>
2026-04-17 22:51:42 +0900

    imrpove AutoWater

--- src/main/java/com/github/kaguya/module/modules/AutoWater.java
+++ src/main/java/com/github/kaguya/module/modules/AutoWater.java
@@ -2,9 +2,7 @@ package com.github.kaguya.module.modules;
 
 import com.github.kaguya.event.EventTarget;
 import com.github.kaguya.event.types.EventType;
-import com.github.kaguya.events.LeftClickMouseEvent;
-import com.github.kaguya.events.RightClickMouseEvent;
-import com.github.kaguya.events.TickEvent;
+import com.github.kaguya.events.UpdateEvent;
 import com.github.kaguya.mixins.IAccessorPlayerControllerMP;
 import com.github.kaguya.module.Module;
 import com.github.kaguya.util.PacketUtil;
@@ -12,14 +10,26 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
-import net.minecraft.util.BlockPos;
 
 public class AutoWater extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
     private int prevSlot = -1;
+    private boolean wantToPlace = false;
+    private boolean wantToCollect = false;
+    private boolean collecting = false;
+    private int collectDelay = 0;
 
     public AutoWater() {
         super("AutoWater", false);
+    }
+
+    @Override
+    public void onDisabled() {
+        prevSlot = -1;
+        wantToPlace = false;
+        wantToCollect = false;
+        collecting = false;
+        collectDelay = 0;
     }
 
     private int findWaterBucketSlot() {
@@ -32,53 +42,63 @@ public class AutoWater extends Module {
         return -1;
     }
 
-    public boolean isSwitching() {
-        return this.prevSlot != -1;
+    private int findEmptyBucketSlot() {
+        for (int i = 0; i < 9; i++) {
+            ItemStack stack = mc.thePlayer.inventory.getStackInSlot(i);
+            if (stack != null && stack.getItem() == Items.bucket) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @EventTarget
-    public void onTick(TickEvent event) {
-        if (!this.isEnabled()) {
-            this.prevSlot = -1;
-            return;
-        }
+    public void onUpdate(UpdateEvent event) {
+        if (!this.isEnabled() || mc.thePlayer == null || mc.theWorld == null) return;
 
-        switch (event.getType()) {
-            case PRE:
-                if (this.prevSlot == -1 && mc.thePlayer.isBurning()) {
-                    int slot = findWaterBucketSlot();
+        if (event.getType() == EventType.PRE) {
+            if (!collecting && prevSlot == -1 && mc.thePlayer.isBurning()) {
+                int slot = findWaterBucketSlot();
+                if (slot != -1) {
+                    prevSlot = mc.thePlayer.inventory.currentItem;
+                    mc.thePlayer.inventory.currentItem = slot;
+                    ((IAccessorPlayerControllerMP) mc.playerController).callSyncCurrentPlayItem();
+                    wantToPlace = true;
+                    event.setRotation(mc.thePlayer.rotationYaw, 89.9F, 50);
+                }
+            } else if (collecting) {
+                if (collectDelay > 0) {
+                    collectDelay--;
+                } else {
+                    int slot = findEmptyBucketSlot();
                     if (slot != -1) {
-                        this.prevSlot = mc.thePlayer.inventory.currentItem;
+                        prevSlot = mc.thePlayer.inventory.currentItem;
                         mc.thePlayer.inventory.currentItem = slot;
                         ((IAccessorPlayerControllerMP) mc.playerController).callSyncCurrentPlayItem();
-                        BlockPos below = new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY - 1, mc.thePlayer.posZ);
-                        PacketUtil.sendPacket(new C08PacketPlayerBlockPlacement(
-                                below, 1, mc.thePlayer.inventory.getCurrentItem(), 0.5f, 1.0f, 0.5f));
+                        wantToCollect = true;
+                        event.setRotation(mc.thePlayer.rotationYaw, 89.9F, 50);
+                    } else {
+                        collecting = false;
                     }
                 }
-                break;
-            case POST:
-                if (this.prevSlot != -1) {
-                    mc.thePlayer.inventory.currentItem = this.prevSlot;
-                    ((IAccessorPlayerControllerMP) mc.playerController).callSyncCurrentPlayItem();
-                    this.prevSlot = -1;
-                }
-                break;
-        }
-    }
-
-    // スイッチ中に誤クリックしないようガード
-    @EventTarget
-    public void onLeftClick(LeftClickMouseEvent event) {
-        if (this.isEnabled() && this.isSwitching()) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventTarget
-    public void onRightClick(RightClickMouseEvent event) {
-        if (this.isEnabled() && this.isSwitching()) {
-            event.setCancelled(true);
+            }
+        } else if (event.getType() == EventType.POST) {
+            if (wantToPlace) {
+                wantToPlace = false;
+                PacketUtil.sendPacket(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()));
+                mc.thePlayer.inventory.currentItem = prevSlot;
+                ((IAccessorPlayerControllerMP) mc.playerController).callSyncCurrentPlayItem();
+                prevSlot = -1;
+                collecting = true;
+                collectDelay = 2;
+            } else if (wantToCollect) {
+                wantToCollect = false;
+                PacketUtil.sendPacket(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()));
+                mc.thePlayer.inventory.currentItem = prevSlot;
+                ((IAccessorPlayerControllerMP) mc.playerController).callSyncCurrentPlayItem();
+                prevSlot = -1;
+                collecting = false;
+            }
         }
     }
 }

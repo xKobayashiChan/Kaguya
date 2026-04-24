feebe4b274a22b3062ece258d9069989c487f832
Lexwtf <120677031+Lexwtf@users.noreply.github.com>
2026-04-25 00:45:02 +0900

    add SpeedMine2

--- src/main/java/com/github/kaguya/module/modules/SpeedMine.java
+++ src/main/java/com/github/kaguya/module/modules/SpeedMine.java
@@ -9,11 +9,14 @@ import com.github.kaguya.property.properties.IntProperty;
 import com.github.kaguya.property.properties.PercentProperty;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.MovingObjectPosition.MovingObjectType;
+import java.util.Random;
 
 public class SpeedMine extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
+    private static final Random RANDOM = new Random();
     public final PercentProperty speed = new PercentProperty("speed", 15);
     public final IntProperty delay = new IntProperty("delay", 0, 0, 4);
+    public final PercentProperty chance = new PercentProperty("chance", 100);
 
     public SpeedMine() {
         super("SpeedMine", false);
@@ -23,7 +26,8 @@ public class SpeedMine extends Module {
     public void onTick(TickEvent event) {
         if (this.isEnabled() && event.getType() == EventType.PRE) {
             if (!mc.playerController.isInCreativeMode()) {
-                if (mc.objectMouseOver != null && mc.objectMouseOver.typeOfHit == MovingObjectType.BLOCK) {
+                if (mc.objectMouseOver != null && mc.objectMouseOver.typeOfHit == MovingObjectType.BLOCK
+                        && RANDOM.nextInt(100) < this.chance.getValue()) {
                     ((IAccessorPlayerControllerMP) mc.playerController)
                             .setBlockHitDelay(Math.min(((IAccessorPlayerControllerMP) mc.playerController).getBlockHitDelay(), this.delay.getValue() + 1));
                     if (((IAccessorPlayerControllerMP) mc.playerController).getIsHittingBlock()) {
